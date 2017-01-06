09b80d96dfa7fd5b08f21a2bcb227dbebdacaee5
CalicoBay <cornelis@calicobay.com>
2017-01-06 12:38:08 -0800

    OnCopyButton in ObjTreeDlg was not offseting properly. The final iteration of the for loop incremented the offset during the last test.

--- ObjTreeDlg.cpp
+++ ObjTreeDlg.cpp
@@ -75,116 +75,135 @@ END_MESSAGE_MAP()
 
 void CObjTreeDlg::OnCopyButton() 
 {
-	HTREEITEM hTreeItem = m_ObjTree.GetSelectedItem();
-	TV_ITEM TreeCtrlItem;
-	TreeCtrlItem.mask = TVIF_HANDLE	| TVIF_PARAM;
-	TreeCtrlItem.hItem = hTreeItem;
+   HTREEITEM hTreeItem = m_ObjTree.GetSelectedItem();
+   TV_ITEM TreeCtrlItem;
+   TreeCtrlItem.mask = TVIF_HANDLE | TVIF_PARAM;
+   TreeCtrlItem.hItem = hTreeItem;
 
-	if (m_ObjTree.GetParentItem(hTreeItem))
-	{
-		m_ObjTree.GetItem(&TreeCtrlItem);
-		CGLObjects* pObject = (CGLObjects*)TreeCtrlItem.lParam;
-		if (pObject)
-		{
-			CRepeatDialog dlg;
-			INT_PTR response = dlg.DoModal();
-			if (response == IDOK)
-			{
-				double offset[3];
-				offset[0]= dlg.m_dblXOffset;
-				offset[1]= dlg.m_dblYOffset;
-				offset[2]= dlg.m_dblZOffset;
-				if (m_pCompObj)
-				{
-					for(UINT i = 1; i <= dlg.m_nCopies; i++,
-						offset[0]+=dlg.m_dblXOffset, offset[1]+=dlg.m_dblYOffset,
-							offset[2]+=dlg.m_dblZOffset)
-					{
-						CGLObjects* pCopied_Object = pObject->Repeat(1, offset, NULL);
-						if (pCopied_Object)
-						{
-							int n_Selection
-								= m_pCCompositeDlg->m_ObjectList.AddString(pCopied_Object->GetDescriptor());
-							if (n_Selection == LB_ERR)
-							{
-								AfxMessageBox(_T("Error adding to Composite list"));
-								return;
-							}
-							m_pCCompositeDlg->m_ObjectList.SetItemDataPtr(n_Selection, (void*)pCopied_Object);
-							m_pCompObj->m_CompList.AddTail(pCopied_Object);
-						}
-					}
-				}
-				else
-					pObject->Repeat(dlg.m_nCopies, offset, m_pActiveDoc);
-			}
-			else
-				return;
-		}
-		else
-		{
-			AfxMessageBox(_T("Item returned a NULL pointer."));
-			return;
-		}
-	}
-	else
-	{
-		m_ObjTree.GetItem(&TreeCtrlItem);
-		CGL_TwoDoc* pDoc = (CGL_TwoDoc*)TreeCtrlItem.lParam;
-		CRepeatDialog dlg;
-		INT_PTR response = dlg.DoModal();
-		double offset[3];
-		if (response == IDOK)
-		{
-			offset[0]= dlg.m_dblXOffset;
-			offset[1]= dlg.m_dblYOffset;
-			offset[2]= dlg.m_dblZOffset;
-		}
-		else
-			return;
-		CDrawObjList* DocObjs = pDoc->GetObjects();
-		if (DocObjs)
-		{
-			POSITION pos = DocObjs->GetHeadPosition();
-			if (!pos)
-			{
-				CString message = pDoc->GetTitle();
-				message.Format(_T("No objects in %s"), message);
-				AfxMessageBox(message);
-				return;
-			}
-			while (pos !=NULL)
-			{
-				CGLObjects* pObject = DocObjs->GetNext(pos);
-				if (pObject)
-				{
-					if (m_pCompObj)
-					{
-					for(UINT i = 1; i <= dlg.m_nCopies; i++,
-						offset[0]+=dlg.m_dblXOffset, offset[1]+=dlg.m_dblYOffset,
-							offset[2]+=dlg.m_dblZOffset)
-						{
-							CGLObjects* pCopied_Object = pObject->Repeat(1, offset, NULL);
-							if (pCopied_Object)
-							{
-								int n_Selection
-									= m_pCCompositeDlg->m_ObjectList.AddString(pCopied_Object->GetDescriptor());
-								if (n_Selection == LB_ERR)
-								{
-									AfxMessageBox(_T("Error adding to Composite list"));
-									return;
-								}
-								m_pCCompositeDlg->m_ObjectList.SetItemDataPtr(n_Selection, (void*)pCopied_Object);
-								m_pCompObj->m_CompList.AddTail(pCopied_Object);
-							}
-						}
-					}
-					else
-						pObject->Repeat(dlg.m_nCopies, offset, m_pActiveDoc);				
-				}
-			}
-		}
-	}
+   if(m_ObjTree.GetParentItem(hTreeItem))
+   {
+      m_ObjTree.GetItem(&TreeCtrlItem);
+      CGLObjects* pObject = (CGLObjects*)TreeCtrlItem.lParam;
+      if(__nullptr != pObject)
+      {
+         CRepeatDialog dlg;
+         INT_PTR response = dlg.DoModal();
+         if(IDOK == response)
+         {
+            double offset[3];
+            offset[0] = dlg.m_dblXOffset;
+            offset[1] = dlg.m_dblYOffset;
+            offset[2] = dlg.m_dblZOffset;
+            if(__nullptr != m_pCompObj)
+            {
+               for(UINT i = 1; i <= dlg.m_nCopies; ++i)
+               {
+                  CGLObjects* pCopied_Object = pObject->Repeat(1, offset, NULL);
+                  if(__nullptr != pCopied_Object)
+                  {
+                     int n_Selection
+                        = m_pCCompositeDlg->m_ObjectList.AddString(pCopied_Object->GetDescriptor());
+                     if(n_Selection == LB_ERR)
+                     {
+                        AfxMessageBox(_T("Error adding to Composite list"));
+                        return;
+                     }
+                     m_pCCompositeDlg->m_ObjectList.SetItemDataPtr(n_Selection, (void*)pCopied_Object);
+                     m_pCompObj->m_CompList.AddTail(pCopied_Object);
+                  }
+                  if(1UL == dlg.m_nCopies)
+                  {
+                     break;
+                  }
+                  offset[0] += dlg.m_dblXOffset;
+                  offset[1] += dlg.m_dblYOffset;
+                  offset[2] += dlg.m_dblZOffset;
+               }
+            }
+            else
+               pObject->Repeat(dlg.m_nCopies, offset, m_pActiveDoc);
+         }
+         else
+            return;
+      }
+      else
+      {
+         AfxMessageBox(_T("Item returned a NULL pointer."));
+         return;
+      }
+   }
+   else
+   {
+      m_ObjTree.GetItem(&TreeCtrlItem);
+      CGL_TwoDoc* pDoc = (CGL_TwoDoc*)TreeCtrlItem.lParam;
+      if(m_pActiveDoc == pDoc)
+      {
+         AfxMessageBox(_T("Copying a whole document on to itself causes recursion!"));
+         return;
+      }
+      CRepeatDialog dlg;
+      INT_PTR response = dlg.DoModal();
+      double offset[3];
+      if(response == IDOK)
+      {
+         offset[0] = dlg.m_dblXOffset;
+         offset[1] = dlg.m_dblYOffset;
+         offset[2] = dlg.m_dblZOffset;
+      }
+      else
+      {
+         return;
+      }
+      CDrawObjList* DocObjs = pDoc->GetObjects();
+      if(DocObjs)
+      {
+         POSITION pos = DocObjs->GetHeadPosition();
+         if(__nullptr == pos)
+         {
+            CString message = pDoc->GetTitle();
+            message.Format(_T("No objects in %s"), message);
+            AfxMessageBox(message);
+            return;
+         }
+         while(__nullptr != pos)
+         {
+            CGLObjects* pObject = DocObjs->GetNext(pos);
+            if(__nullptr != pObject)
+            {
+               if(__nullptr != m_pCompObj)
+               {
+                  for(UINT i = 1; i <= dlg.m_nCopies; ++i)
+                  {
+                     CGLObjects* pCopied_Object = pObject->Repeat(1, offset, NULL);
+                     if(__nullptr != pCopied_Object)
+                     {
+                        int n_Selection
+                           = m_pCCompositeDlg->m_ObjectList.AddString(pCopied_Object->GetDescriptor());
+                        if(n_Selection == LB_ERR)
+                        {
+                           AfxMessageBox(_T("Error adding to Composite list"));
+                           return;
+                        }
+                        m_pCCompositeDlg->m_ObjectList.SetItemDataPtr(n_Selection, (void*)pCopied_Object);
+                        m_pCompObj->m_CompList.AddTail(pCopied_Object);
+                     }
+                     if(1UL == dlg.m_nCopies)
+                     {
+                        break;
+                     }
+                     offset[0] += dlg.m_dblXOffset;
+                     offset[1] += dlg.m_dblYOffset;
+                     offset[2] += dlg.m_dblZOffset;
+                  }
+               }
+               else
+               {
+                  pObject->Repeat(dlg.m_nCopies, offset, m_pActiveDoc);
+               }
+            }
+         }
+      }
+   }
 }
 
 BOOL CObjTreeDlg::OnInitDialog() 
